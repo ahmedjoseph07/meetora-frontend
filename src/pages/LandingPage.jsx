df7ac12f2c374e2adfa24435509d39f30f89874c
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2024-11-30 18:19:17 +0600

    landing-page design

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import React from 'react';
+import {Link} from "react-router-dom";
+import "../App.css";
 const LandingPage = () => {
     return (
-        <div>
-            <h1>Landing Page</h1>
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'><h2>Meetora</h2></div>
+                <div className='navList'>
+                    <p>Join as a guest</p>
+                    <p>Register</p>
+                    <div role='button'>Login</div>
+                </div>
+            </nav>
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span className='connectText'>Connect</span> with your loved ones</h1>
+                    <p>Best solution for chats, Audio and Video calls..</p>
+                    <div role='button' className='linkBtnDiv'><Link to="/home" className='linkBtn'>Get Started</Link></div>
+                </div>
+                <div>
+                    <img src="/mobile.png" alt="" />
+                </div>
+            </div>
         </div>
     )
 }
 
-export default LandingPage
+export default LandingPage;
